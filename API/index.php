1732c391ed86c47a15bd4a48c0d41478ea52d00f
unknown <jarce2frise@yahoo.fr>
2020-06-05 10:49:58 +0200

    First commit

---
+++ API/index.php
@@ -0,0 +1,28 @@
+<?php 
+// $_SERVER['REQUEST_METHOD']               retour la méthode
+// $_REQUEST                                Stock toutes les données
+// file_get_contents('php://input')         Pour stocker les données POST
+// json_decode($json)                       Decode en Objet php
+// son_encode($jsonObj)                     Encode un tableau en objet JSON 
+
+
+    /*
+    //Method POST
+    header('Access-Control-Allow-Origin: *');
+
+    // Reading JSON POST using PHP
+    $json = file_get_contents('php://input');
+    $jsonObj = json_decode($json);
+
+    echo json_encode($jsonObj);
+
+    */
+
+
+    //Method GET
+    header('Access-Control-Allow-Origin: *');
+    $data = $_REQUEST;
+    $data["method"]=$_SERVER['REQUEST_METHOD'];
+
+    echo json_encode($data);
+?>
